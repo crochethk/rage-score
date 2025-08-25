1300eb3f2de620132244d8e2b2b3ad74efe70f8d
Rafael Szydelko <70408499+crochethk@users.noreply.github.com>
2025-08-25 22:37:20 +0200

    Set up Prettier and add vscode extension recommendations

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,18 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
-import { globalIgnores } from 'eslint/config';
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { globalIgnores } from "eslint/config";
 
-import reactX from 'eslint-plugin-react-x';
-import reactDom from 'eslint-plugin-react-dom';
+import reactX from "eslint-plugin-react-x";
+import reactDom from "eslint-plugin-react-dom";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default tseslint.config([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
 
@@ -23,17 +24,17 @@ export default tseslint.config([
       // Stylistic rules (optional)
       ...tseslint.configs.stylisticTypeChecked,
 
-      reactHooks.configs['recommended-latest'],
+      reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
 
       // Enable lint rules for React
-      reactX.configs['recommended-typescript'],
+      reactX.configs["recommended-typescript"],
       // Enable lint rules for React DOM
       reactDom.configs.recommended,
     ],
     languageOptions: {
       parserOptions: {
-        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
         // @ts-ignore
         tsconfigRootDir: import.meta.dirname,
       },
@@ -41,7 +42,8 @@ export default tseslint.config([
       globals: globals.browser,
     },
     rules: {
-      'semi': ['error', 'always'],
+      semi: ["error", "always"],
     },
   },
+  eslintConfigPrettier,
 ]);
