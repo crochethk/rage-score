ac4eac86e6bcf8bc3c51f815c3bd25a597bc78b6
Rafael Szydelko <70408499+crochethk@users.noreply.github.com>
2025-09-11 22:06:46 +0200

    Reiterate ScoreInputDialog draft

--- src/ScoreInputDialog.tsx
+++ src/ScoreInputDialog.tsx
@@ -1,4 +1,4 @@
-import { Button, Col, Form, Row } from "react-bootstrap";
+import { Button, ButtonGroup, Col, Form, Row } from "react-bootstrap";
 
 const range = (start: number, stop: number, step = 1) => {
   return Array.from(
@@ -17,73 +17,98 @@ export function ScoreInputDialog() {
 
   return (
     <>
-      <Row className="text-nowrap text-center align-items-center">
-        <Col xs="12" md="2">
-          <Button size="lg" variant="primary">
-            ← PREV
-          </Button>
-        </Col>
+      <Row className="text-center justify-content-center">
+        <Col sm="8" style={{ maxWidth: "576px" }}>
+          <h5>Runde 123</h5>
+          <h3>Max Musterfrau</h3>
+          <Form.Floating>
+            <Form.FloatingLabel
+              label="Gewettet"
+              controlId="bidInput"
+              className="my-2"
+            >
+              <Form.Select
+                aria-label="Gewettete Stiche"
+                required
+                className="text-center"
+              >
+                <option value="" selected>
+                  ---
+                </option>
+                {possibleBidsOptions}
+              </Form.Select>
+            </Form.FloatingLabel>
 
-        <Col>
-          <Row>
-            <Col xs="12">Round XXX</Col>
-            <Col xs="12">Valentino Vera</Col>
-            <Col xs className="mx-auto">
-              <Form.Floating>
-                <Form.FloatingLabel label="Gewettet" controlId="bidInput">
-                  <Form.Select aria-label="Auswahl gewetteter Stiche" required>
-                    <option value="" selected>
-                      ---
-                    </option>
-                    {possibleBidsOptions}
-                  </Form.Select>
-                </Form.FloatingLabel>
+            <Form.FloatingLabel
+              label="Bekommen"
+              controlId="tricksInput"
+              className="my-2"
+            >
+              <Form.Select
+                aria-label="Bekommene Stiche"
+                required
+                className="text-center"
+              >
+                <option value="" selected>
+                  ---
+                </option>
+                {possibleBidsOptions}
+              </Form.Select>
+            </Form.FloatingLabel>
 
-                <Form.FloatingLabel label="Erhalten" controlId="tricksInput">
-                  <Form.Select aria-label="Auswahl erhaltener Stiche" required>
-                    <option value="" selected>
-                      ---
-                    </option>
-                    {possibleBidsOptions}
-                  </Form.Select>
-                </Form.FloatingLabel>
+            <Form.FloatingLabel
+              label="Bonus/Malus aus Karten"
+              controlId="cardPointsInput"
+              className="my-2"
+            >
+              <Form.Select
+                aria-label="Bonuspunkte"
+                defaultValue={0}
+                className="text-center"
+              >
+                {range(3 * -5, 3 * 5 + 1, 5).map((p) => (
+                  <option key={p} value={p}>
+                    {p > 0 ? "+" + p : p}
+                  </option>
+                ))}
+              </Form.Select>
+            </Form.FloatingLabel>
+          </Form.Floating>
 
-                <Form.FloatingLabel
-                  label="Bonus/Malus"
-                  controlId="cardPointsInput"
-                >
-                  <Form.Select
-                    aria-label="Auswahl Bonuspunkte"
-                    defaultValue={0}
-                  >
-                    {range(3 * -5, 3 * 5 + 1, 5).map((p) => (
-                      <option key={p} value={p}>
-                        {p > 0 ? "+" + p : p}
-                      </option>
-                    ))}
-                  </Form.Select>
-                </Form.FloatingLabel>
-              </Form.Floating>
-
-              <div id="roundPointsDisplay">
-                <i>waiting for input...</i>
-              </div>
-            </Col>
-          </Row>
-        </Col>
-
-        <Col xs="12" md="2">
-          <Button size="lg" variant="primary">
-            NEXT →
-          </Button>
+          <div id="roundPointsDisplay">
+            <i>Warte auf Eingaben...</i>
+          </div>
         </Col>
       </Row>
 
-      <Row className="mt-2">
-        <Col className="text-center">
-          <Button size="lg" variant="secondary">
-            DONE
-          </Button>
+      <Row className="mt-2 justify-content-center">
+        <Col sm="8" style={{ maxWidth: "576px" }}>
+          <div className="d-grid">
+            <ButtonGroup vertical>
+              <ButtonGroup size="lg" aria-label="Navigations-Buttons">
+                <Button
+                  variant="primary"
+                  className="border border-primary-subtle border-1"
+                  aria-label="Zum vorherigen Spieler"
+                >
+                  ←
+                </Button>
+                <Button
+                  variant="primary"
+                  className="border border-primary-subtle border-1"
+                  aria-label="Zum nächsten Spieler"
+                >
+                  →
+                </Button>
+              </ButtonGroup>
+              <Button
+                variant="secondary"
+                className="border border-secondary-subtle border-1"
+              >
+                Fertig
+              </Button>
+            </ButtonGroup>
+          </div>
         </Col>
       </Row>
     </>
