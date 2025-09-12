339f09ea355209a24fe6a4603fa8ed0d7fec9ab2
Rafael Szydelko <70408499+crochethk@users.noreply.github.com>
2025-09-12 16:08:18 +0200

    Refactor ScoreInputDialog - Extract ScoreInputForm - Extract PrimaryDialogButton and SecondaryDialogButton components - Introduce new folder structure for components     - Move ScoreInputDialog.tsx accordingly

---
+++ src/components/dialogs/ScoreInputDialog.tsx
@@ -0,0 +1,112 @@
+import { ButtonGroup, Col, Form, Row } from "react-bootstrap";
+import {
+  PrimaryDialogButton,
+  SecondaryDialogButton,
+} from "../ui/Button/DialogButton";
+
+const range = (start: number, stop: number, step = 1) => {
+  return Array.from(
+    { length: Math.ceil((stop - start) / step) },
+    (_, i) => start + i * step,
+  );
+};
+
+export function ScoreInputDialog() {
+  return (
+    <>
+      <Row className="text-center justify-content-center">
+        <Col sm="8" style={{ maxWidth: "576px" }}>
+          <h5>Runde 123</h5>
+          <h3>Max Musterfrau</h3>
+          <ScoreInputForm />
+
+          <div id="roundPointsDisplay">
+            <i>Warte auf Eingaben...</i>
+          </div>
+        </Col>
+      </Row>
+
+      <Row className="mt-2 justify-content-center">
+        <Col sm="8" style={{ maxWidth: "576px" }}>
+          <div className="d-grid">
+            <ButtonGroup vertical>
+              <ButtonGroup size="lg" aria-label="Navigations-Buttons">
+                <PrimaryDialogButton aria-label="Nächster Spieler">
+                  ←
+                </PrimaryDialogButton>
+                <PrimaryDialogButton aria-label="Vorheriger Spieler">
+                  →
+                </PrimaryDialogButton>
+              </ButtonGroup>
+              <SecondaryDialogButton>Fertig</SecondaryDialogButton>
+            </ButtonGroup>
+          </div>
+        </Col>
+      </Row>
+    </>
+  );
+}
+
+function ScoreInputForm() {
+  // TODO the range should be based on passed props, so it depends on dealt cards
+  const possibleBidsOptions = range(0, 10 + 1).map((b) => (
+    <option key={b} value={b}>
+      {b}
+    </option>
+  ));
+  return (
+    <Form.Floating>
+      <Form.FloatingLabel
+        label="Gewettet"
+        controlId="bidInput"
+        className="my-2"
+      >
+        <Form.Select
+          aria-label="Gewettete Stiche"
+          required
+          className="text-center"
+        >
+          <option value="" selected>
+            ---
+          </option>
+          {possibleBidsOptions}
+        </Form.Select>
+      </Form.FloatingLabel>
+
+      <Form.FloatingLabel
+        label="Bekommen"
+        controlId="tricksInput"
+        className="my-2"
+      >
+        <Form.Select
+          aria-label="Bekommene Stiche"
+          required
+          className="text-center"
+        >
+          <option value="" selected>
+            ---
+          </option>
+          {possibleBidsOptions}
+        </Form.Select>
+      </Form.FloatingLabel>
+
+      <Form.FloatingLabel
+        label="Bonus/Malus aus Karten"
+        controlId="cardPointsInput"
+        className="my-2"
+      >
+        <Form.Select
+          aria-label="Bonuspunkte"
+          defaultValue={0}
+          className="text-center"
+        >
+          {range(3 * -5, 3 * 5 + 1, 5).map((p) => (
+            <option key={p} value={p}>
+              {p > 0 ? "+" + p : p}
+            </option>
+          ))}
+        </Form.Select>
+      </Form.FloatingLabel>
+    </Form.Floating>
+  );
+}
