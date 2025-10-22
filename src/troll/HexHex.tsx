564d74dcf0966ee8d2daa0a29f3d99c894f00a47
Rafael Szydelko <70408499+crochethk@users.noreply.github.com>
2025-10-23 00:59:53 +0200

    feat(troll): add thrusting animation

--- src/troll/HexHex.tsx
+++ src/troll/HexHex.tsx
@@ -1,10 +1,38 @@
 import "./HexHex.css";
+import "./Thruster.css";
 import hexhexImg from "./hexhex.png";
 
 export function HexHex({ show }: { show: boolean }) {
   return (
     <>
-      {show && <img className="hexhex" src={hexhexImg} />}
+      {show && (
+        <div className="hexhex">
+          <img src={hexhexImg} />
+          <Thruster />
+        </div>
+      )}
     </>
+  );
+}
+
+function Thruster() {
+  return (
+    <div className="thruster" aria-hidden="true">
+      <span className="particle" />
+      <span className="particle" />
+      <span className="particle" />
+      <span className="particle" />
+      <span className="particle" />
+      <span className="particle" />
+      <span className="particle" />
+      <span className="particle" />
+      <span className="particle" />
+      <span className="particle" />
+      <span className="particle" />
+      <span className="particle" />
+      <span className="particle" />
+      <span className="particle" />
+      <span className="particle" />
+    </div>
   );
 }
