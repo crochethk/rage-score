49a9e1c9e83fb1e54988d3ff26b65f3876425cd5
Rafael Szydelko <70408499+crochethk@users.noreply.github.com>
2025-12-19 21:18:23 +0100

    feat(useLocalStorage): synchronize state across tabs/windows
    Prior to this, changes made in multiple windows/tabs would not be applied to each other's app state which might lead to inconsistencies.
    This commit solves this by adding a `storage` event listener which automatically updates the app state from `localStorage`.

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -2,28 +2,51 @@ import { useEffect, useState } from "react";
 
 export function useLocalStorage<T>(
   key: string,
-  defaultState: T,
-  onLoad?: (o: T) => T,
+  defaultValue: T,
+  onInitialLoad?: (o: T) => T,
 ) {
   const [state, setState] = useState<T>(() => {
-    // Try to initialize to state stored in localStorage
+    const storedJson = localStorage.getItem(key);
+    if (storedJson === null) {
+      // no previous value
+      return defaultValue;
+    }
+    // try parse stored value
     try {
-      const storedJson = localStorage.getItem(key);
-      if (storedJson) {
-        const loaded = JSON.parse(storedJson) as T;
-        return onLoad ? onLoad(loaded) : loaded;
-      } else {
-        return defaultState;
-      }
+      const loaded = JSON.parse(storedJson) as T;
+      return onInitialLoad ? onInitialLoad(loaded) : loaded;
     } catch (error) {
-      console.error("Failed to parse stored JSON from localStorage", error);
-      return defaultState;
+      console.error(`Failed to parse '${key}': '${storedJson}'.`, error);
+      return defaultValue;
     }
   });
 
   useEffect(() => {
     localStorage.setItem(key, JSON.stringify(state));
   }, [key, state]);
+
+  // Listen to storage events to sync state across tabs/windows
+  useEffect(() => {
+    const syncHandler = (e: StorageEvent) => {
+      if (e.storageArea === localStorage && e.key === key) {
+        if (e.newValue === null) {
+          console.warn(
+            `Ignored external localStorage change: key "${key}" was removed, which is not supported.`,
+          );
+          return;
+        }
+
+        try {
+          setState(JSON.parse(e.newValue) as T);
+        } catch (error) {
+          console.error(`Failed to parse '${e.key}': '${e.newValue}'.`, error);
+          return;
+        }
+      }
+    };
+    window.addEventListener("storage", syncHandler);
+    return () => window.removeEventListener("storage", syncHandler);
+  }, [key]);
 
   return [state, setState] as const;
 }
