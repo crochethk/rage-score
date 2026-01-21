142ff38547ed813916d5112cacba59ccaa6bcb40
Rafael Szydelko <70408499+crochethk@users.noreply.github.com>
2026-01-21 17:29:31 +0100

    feat(spectator): add basic routes

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
 import App from "./App.tsx";
 
 import "bootstrap-icons/font/bootstrap-icons.min.css";
@@ -8,6 +9,21 @@ import "./main.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          index
+          element={<App />} // TODO add HostApp component
+        />
+        <Route
+          path="/spectate/:roomId"
+          element={<h1>Route "spectate/:roomId" not implemented, yet</h1>} // TODO add SpectatorApp component
+        />
+        <Route
+          path="*"
+          element={<h1>404 Not Found</h1>} // TODO add Error404 component
+        />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
