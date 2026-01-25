81d8a5bf169118a27ae08bf7750025f220e47bee
Rafael Szydelko <70408499+crochethk@users.noreply.github.com>
2026-01-25 16:37:50 +0100

    feat(AppSpectator): use mock data to simulate rooms

--- apps/web/src/AppSpectator.tsx
+++ apps/web/src/AppSpectator.tsx
@@ -1,12 +1,31 @@
 import { useState } from "react";
 import { useParams } from "react-router";
 import App from "./App";
+import * as mock from "./exampleData";
 import { useGameState } from "./hooks/useGameState";
 import type { GameData } from "./types";
 
+// TODO: remove Mock rooms with example data
+const rooms = new Map<string, GameData>([
+  ["1", mock.dataSet1],
+  ["2", mock.dataSet2],
+  ["3", mock.dataSet3],
+  ["4", mock.dataSet4],
+  ["5", mock.dataSet5],
+  ["42", mock.dataSet42],
+]);
+
+const emptyData = { players: [], rounds: [] };
+
 export function AppSpectator() {
   const { roomId } = useParams();
-  const [data, setData] = useState<GameData>({ players: [], rounds: [] });
+  const [data, setData] = useState<GameData>(() => {
+    const d = rooms.get(roomId ?? "");
+    if (!d) {
+      return emptyData;
+    }
+    return d;
+  });
   const gs = useGameState(() => data, setData);
 
   // TODO add socket connection logic for spectating a game
