bf1bc47a5fec7495d57dd3690dd98a9c808089f2
Rafael Szydelko <70408499+crochethk@users.noreply.github.com>
2026-01-27 00:23:17 +0100

    chore(monorepo): update eslint config
    - add root eslint config
    - in `apps/web/eslint.config.js`:
        - use `projectService` parser option: It's the more recent and convenient way to provide type information to eslint especially in monorepos. See https://typescript-eslint.io/blog/announcing-typescript-eslint-v8-beta/#project-service
        - reorganize imports
        - migrate `tseslint.config` to eslint's `defineConfig` utility
    - apply `eslint-config-prettier` in the root config

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,15 +1,13 @@
 import js from "@eslint/js";
-import globals from "globals";
+import reactDom from "eslint-plugin-react-dom";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-import { globalIgnores } from "eslint/config";
-
 import reactX from "eslint-plugin-react-x";
-import reactDom from "eslint-plugin-react-dom";
-import eslintConfigPrettier from "eslint-config-prettier/flat";
+import { defineConfig, globalIgnores } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
-export default tseslint.config([
+export default defineConfig([
   globalIgnores(["dist"]),
   {
     files: ["**/*.{ts,tsx}"],
@@ -34,7 +32,7 @@ export default tseslint.config([
     ],
     languageOptions: {
       parserOptions: {
-        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+        projectService: true,
         // @ts-ignore
         tsconfigRootDir: import.meta.dirname,
       },
@@ -46,7 +44,6 @@ export default tseslint.config([
       "@typescript-eslint/no-unused-vars": ["warn"],
     },
   },
-  eslintConfigPrettier,
   {
     files: ["**/contexts/**/*.tsx", "**/contexts/**/*.ts"],
     rules: {
---
+++ eslint.config.js
@@ -0,0 +1,4 @@
+import eslintConfigPrettier from "eslint-config-prettier";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([eslintConfigPrettier]);
