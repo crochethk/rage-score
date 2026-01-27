34ca02597c600640d31ca284e20128f44bfb9e5b
Rafael Szydelko <70408499+crochethk@users.noreply.github.com>
2026-01-27 21:16:21 +0100

    feat(relay-server): init project

---
+++ apps/relay-server/eslint.config.js
@@ -0,0 +1,29 @@
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+export default defineConfig(
+  js.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      semi: ["error", "always"],
+      "@typescript-eslint/no-unused-vars": ["off"],
+      "@typescript-eslint/switch-exhaustiveness-check": ["error"],
+    },
+  },
+  {
+    files: ["**/*.js"],
+    ...tseslint.configs.disableTypeChecked,
+  },
+  {
+    ignores: ["dist/", "node_modules/"],
+  },
+);
