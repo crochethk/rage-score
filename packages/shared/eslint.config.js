531c495b08777e26b74775a85c04c722fe322418
Rafael Szydelko <70408499+crochethk@users.noreply.github.com>
2026-01-31 22:02:09 +0100

    chore(shared): init `packages/shared` project

---
+++ packages/shared/eslint.config.js
@@ -0,0 +1,30 @@
+import js from "@eslint/js";
+import eslint from "eslint";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+export default defineConfig(
+  js.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      semi: ["error", "always"],
+      "@typescript-eslint/no-unused-vars": ["off"],
+      "@typescript-eslint/switch-exhaustiveness-check": ["error"],
+    },
+  },
+  {
+    files: ["**/*.js"],
+    ...tseslint.configs.disableTypeChecked,
+  },
+  {
+    ignores: ["dist/", "node_modules/"],
+  },
+);
---
+++ packages/shared/src/index.ts
@@ -0,0 +1,6 @@
+// TODO remove this
+console.log("hello world from shared package");
+
+export function greet() {
+  return 1;
+}
